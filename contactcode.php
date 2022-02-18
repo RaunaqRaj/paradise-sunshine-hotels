e7207a54ca00a873f6f2da961eff3d48d3996f51
RaunaqRaj <raunaqraj2003@gmail.com>
2022-02-18 16:50:18 +0530

    used new validations for form

--- contactcode.php
+++ contactcode.php
@@ -24,6 +24,20 @@
 
       if(!empty($name)&& !empty($email) && !empty($subject) && !empty($message)){
 
+            if(preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$name)){
+
+                  echo "please enter a valid name";
+
+            }
+            else{
+            if(!preg_match("/^[a-zA-Z]*$/",$name)){
+
+                  echo "name must be a character";
+            }
+      
+            else{
+
+
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                   
                   echo "invalid email address";
@@ -38,9 +52,11 @@
                   echo "unable to send message";
             }
       }
-      }else{
+      }
+      }
+      }     
+      else{
             echo "please fill the above details";
       }
-
 
 ?>
