12a162ac77b03055dc511f82d108f44ca3a60c87
RaunaqRaj <raunaqraj2003@gmail.com>
2022-04-24 20:15:10 +0530

    downloaded bootstrap files offline

--- components/head_start.php
+++ components/head_start.php
@@ -7,8 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="assests/css/bootstrap.css">
   <link rel="stylesheet" href="assests/css/style.css">
   <script src="https://kit.fontawesome.com/76d2de9cd5.js" crossorigin="anonymous"></script>
   <link rel="icon" href="assests/images/P-removebg-preview.png" type="image/x-icon">
@@ -27,17 +26,13 @@
   crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
   <!-- scrollreveal javascript -->
-  <script src="https://unpkg.com/scrollreveal"></script>
-
+  <script src="assests/js/scrollreveal.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/plugins/animation.gsap.min.js"
     integrity="sha512-5/OHwmQzDSBS0Ous4/hlYoWLHd06/d2r7LdKZQVBXOA6PvOqWVXtdboiLTU7lQTGyVoKVTNkwi0ol4gHGlw5ww=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-  <!-- <link rel="stylesheet" href="assests/css/style.css"> -->
+    <script src="assests/js/bootstrap.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="assests/css/enquiry.css">
-  <script src="https://kit.fontawesome.com/76d2de9cd5.js" crossorigin="anonymous"></script>
+  <script src="assests/js/font_awesome.js" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"
     integrity="sha512-z4OUqw38qNLpn1libAN9BsoDx6nbNFio5lA6CuTp9NlK83b89hgyCVq+N5FdBJptINztxn1Z3SaKSKUS5UP60Q=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
